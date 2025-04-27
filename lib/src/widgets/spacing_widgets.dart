3f33b3713d4c649107c94cb505800c3a319020e1
Mohamed draz <61053728+draz26648@users.noreply.github.com>
2025-04-27 14:28:36 +0300

    Update spacing_widgets.dart

--- lib/src/widgets/spacing_widgets.dart
+++ lib/src/widgets/spacing_widgets.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 /// A widget that creates vertical space with a specified height.
 /// We avoid creating them as functions to avoid the need for a context and make them const as possible.
-/// 
+///
 /// This is a convenience widget similar to SizedBox but specifically for
 /// vertical spacing.
 class VerticalSpace extends StatelessWidget {
   /// Creates a widget that adds vertical space.
-  /// 
+  ///
   /// The [height] parameter is required and specifies the amount of vertical space.
   const VerticalSpace({super.key, required this.height});
 
@@ -21,12 +21,12 @@ class VerticalSpace extends StatelessWidget {
 }
 
 /// A widget that creates horizontal space with a specified width.
-/// 
+///
 /// This is a convenience widget similar to SizedBox but specifically for
 /// horizontal spacing.
 class HorizontalSpace extends StatelessWidget {
   /// Creates a widget that adds horizontal space.
-  /// 
+  ///
   /// The [width] parameter is required and specifies the amount of horizontal space.
   const HorizontalSpace({super.key, required this.width});
 
